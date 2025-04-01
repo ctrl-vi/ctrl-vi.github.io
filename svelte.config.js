295a3c67369f9e2d7f919ce7a9f60e9690368f69
Violet Monserate <55568600+ctrl-vi@users.noreply.github.com>
2025-04-01 12:25:50 -0700

    Add PROPER fallback page

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: "app.html",
+			fallback: "404.html",
 			precompress: false,
 			strict: true
 		}),
