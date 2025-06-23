e7e54fcee469d0aa82593d0ed6c81391ec21b7df
Violet Monserate <55568600+ctrl-vi@users.noreply.github.com>
2025-06-23 10:08:15 -0700

    Add analytics :)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ctrl-vi.github.io',
+  site: 'https://ctrl-vi.github.io',
+  integrations: [partytown()],
 });
