c480fbe51966a97d2efc7c71f7ca7046f83ef100
Violet Monserate <55568600+ctrl-vi@users.noreply.github.com>
2025-10-08 00:31:05 -0700

    Add .mdx + embedded Youtube support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import partytown from '@astrojs/partytown';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ctrl-vi.github.io',
-  integrations: [partytown()],
+  integrations: [partytown(), mdx()],
   markdown: {
     syntaxHighlight: "prism",
     remarkPlugins: [remarkMath],
