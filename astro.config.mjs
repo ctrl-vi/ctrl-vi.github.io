b9ffed6b021d77450cd732537592c0f53460a415
Violet Monserate <violetmonserate@gmail.com>
2026-07-24 09:08:33 -0700

    Remove deprecated 'baseUrl' TS config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import partytown from '@astrojs/partytown';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import { fileURLToPath } from 'node:url';
 
 import mdx from '@astrojs/mdx';
 
@@ -10,6 +11,17 @@ import mdx from '@astrojs/mdx';
 export default defineConfig({
   site: 'https://ctrl-vi.github.io',
   integrations: [partytown(), mdx()],
+  vite: {
+    resolve: {
+      alias: {
+        '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+        '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+        '@cardIcons': fileURLToPath(new URL('./src/assets/cardIcons', import.meta.url)),
+        '@public': fileURLToPath(new URL('./public', import.meta.url)),
+        '@styles': fileURLToPath(new URL('./src/styles', import.meta.url))
+      }
+    }
+  },
   markdown: {
     syntaxHighlight: "prism",
     remarkPlugins: [remarkMath],
