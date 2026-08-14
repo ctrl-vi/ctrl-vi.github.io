95709510b0545fb7da7034dea2a8453670feb4bb
Violet Monserate <55568600+ctrl-vi@users.noreply.github.com>
2026-08-13 21:12:15 -0700

    Upgrade Astro and packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import partytown from '@astrojs/partytown';
+import { unified } from "@astrojs/markdown-remark";
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import remarkInline from '@plugins/inline-code';
 import { fileURLToPath } from 'node:url';
 
 import mdx from '@astrojs/mdx';
@@ -10,7 +12,13 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ctrl-vi.github.io',
-  integrations: [partytown(), mdx()],
+  integrations: [
+    partytown(),
+    mdx({
+      remarkPlugins: [remarkInline, remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+  ],
   vite: {
     resolve: {
       alias: {
@@ -18,13 +26,16 @@ export default defineConfig({
         '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
         '@cardIcons': fileURLToPath(new URL('./src/assets/cardIcons', import.meta.url)),
         '@public': fileURLToPath(new URL('./public', import.meta.url)),
-        '@styles': fileURLToPath(new URL('./src/styles', import.meta.url))
+        '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
+        '@plugins': fileURLToPath(new URL('./src/plugins', import.meta.url))
       }
     }
   },
   markdown: {
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
+    processor: unified({
+      remarkPlugins: [remarkInline, remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
     syntaxHighlight: {
       type: 'shiki',
       excludeLangs: []
